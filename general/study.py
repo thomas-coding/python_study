2cc890428a3568a08e3ac0686a76b495f28ea884
Jinping Wu <jinping.wu@verisilicon.com>
2023-10-26 17:24:42 +0800

    add data study

--- general/study.py
+++ general/study.py
@@ -5,9 +5,21 @@
 # This file is part of build
 
 from study_control import study_control_func
+from study_data import study_data_func
 
+def help():
+    print('0 -- control')
+    print('1 -- data')
 
 if __name__ == '__main__':
     print('study main')
-    study_control_func()
+    help()
+    choice = int(input("Please enter an integer: "))
+    if choice == 0:
+        study_control_func()
+    elif choice == 1:
+        study_data_func()
+    else:
+        help()
+
 
--- general/study_data.py
+++ general/study_data.py
@@ -15,12 +15,44 @@ class Study_data:
 
         self.input = input
 
-    def data(self):
-        print('stud data enter')
-        return 0
+    def data_list(self):
+
+        # list
+        stack = [1, 3, 2]
+        stack.append(4)
+        print(stack)
+        value = stack.pop()
+        print(stack)
+        print(value)
+        stack.sort()
+        print(stack)
+        del stack[1]
+        print(stack)
+
+        # dict
+        tel = {'tom':1111, 'lily':2222}
+        print(tel)
+        tel['tom'] = 3333
+        print(tel)
+        tel['test'] = 8888
+        print(tel)
+        if 'test' in tel:
+            print(tel['test'])
+        if 'jack' not in tel:
+            tel['jack'] = 9999
+        print(tel)
+
+        for k, v in tel.items():
+            print(v, end=' ')
+        print('')
+
+        basket = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
+        for f in sorted(set(basket)):
+            print(f)
+        return
 
 def study_data_func():
     my_data = Study_data(1)
-    my_data.data()
-    return 0
+    my_data.data_list()
+    return
 
